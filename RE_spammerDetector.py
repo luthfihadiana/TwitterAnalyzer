3bb6a8c8177b8a14dfbcf05246e5deeb21b73782
rifoag <rifoagenadi@gmail.com>
2018-04-23 00:55:54 +0700

    RE spammer detector yang lebih elegan

--- RE_spammerDetector.py
+++ RE_spammerDetector.py
@@ -4,20 +4,12 @@ import sys
 def keyword_spam(text):
 # mengembalikan 1 jika ditemukan keyword spam pada postingan
 # mengembalikan -1 jika tidak
-	pattern = r'^[hH][tT]{2}[Pp][Ss]'
-	b1 = bool(re.search(pattern,text))
-	pattern = r'[wW][aA][dD][uU][hH]'
-	b2 = bool(re.search(pattern,text))
-	pattern = r'[hH][eE][yY]'
-	b3 = bool(re.search(pattern,text))
-	pattern = r'[Ww][eEiI][bBeE][bBUu]'
-	b4 = bool(re.search(pattern,text))
-	pattern = r'[A-Z][A-Z][A-Z]48'
-	b5 = bool(re.search(pattern,text))	
-	if (b1 or b2 or b3 or b4 or b5) :
-		return 1
-	else:
-		return -1
+	patterns = [r'^[hH][tT]{2}[Pp][Ss]', r'[wW][aA][dD][uU][hH]', r'[hH][eE][yY]', r'[Ww][eEiI][bBeE][bBUu]', r'[A-Z][A-Z][A-Z]48', r'[Mm][Aa][Rr][Ii][Kk][Aa]']
+	for pattern in patterns :
+		if (bool(re.search(pattern,text))) :
+			return 1
+	return -1
 
 # main program
 print(keyword_spam(sys.argv[1]))
+
